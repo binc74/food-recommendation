5f04c9faedd3dcd4b4c6390bf4ebc80429f7e67b
burnstick <zimingmaosu@gmail.com>
2019-01-23 18:36:35 -0500

    modify PrefSetActivity

--- app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/PrefSetActivity.java
+++ app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/PrefSetActivity.java
@@ -1,13 +1,35 @@
 package edu.osu.cse5914.ibmi.foodrecommendation;
 
+import android.arch.lifecycle.ViewModel;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class PrefSetActivity extends AppCompatActivity {
+public class PrefSetActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //private TextView mTextView;
+    private Button msubmitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pref_set);
+        //mTextView = findViewById(R.id.text1);
+        msubmitButton = findViewById(R.id.submit_pref);
+        msubmitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.submit_pref:
+                Intent pref_intent = new Intent(this, LoginActivity.class); //link to preference view
+                startActivity(pref_intent);
+                break;
+        }
     }
 }
