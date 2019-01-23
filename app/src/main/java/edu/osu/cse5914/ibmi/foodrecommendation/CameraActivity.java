de3bfa82da2a7abe6a6c63a1c6c1b27b9aa791f3
Qinghuiz <zqhtim@gmail.com>
2019-01-23 18:23:24 -0500

    Added camera activity

---
+++ app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/CameraActivity.java
@@ -0,0 +1,13 @@
+package edu.osu.cse5914.ibmi.foodrecommendation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
