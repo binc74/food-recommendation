36c9b08a7d39d6e93d6de29222b7645f0fe34e19
Qinghuiz <zqhtim@gmail.com>
2019-01-23 18:35:52 -0500

    added camera activity

--- app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/CameraActivity.java
+++ app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/CameraActivity.java
@@ -1,13 +1,34 @@
 package edu.osu.cse5914.ibmi.foodrecommendation;
 
+import android.arch.lifecycle.ViewModel;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextView;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        mTextView = findViewById(R.id.text1);
+        mButton = findViewById(R.id.take_picture);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.submit_sign_up:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
