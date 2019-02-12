63e65ec1a8542b6c0f9f4ea7cf3ba18cff9fff03
burnstick <zimingmaosu@gmail.com>
2019-02-12 16:34:12 -0500

    visual reccc

--- app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/TestActivity.java
+++ app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/TestActivity.java
@@ -5,31 +5,62 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 
 import java.io.File;
 
-public class TestActivity extends AppCompatActivity {
+import edu.osu.cse5914.ibmi.foodrecommendation.util.VisualRecTask;
+
+public class TestActivity extends AppCompatActivity implements View.OnClickListener{
     private static String TAG = "TestActivity";
 
     private ImageView mImage;
+    private Button mButton;
+    private TextView mTextView;
+
+    private String filePath;
+
+    //private VisualRecognition VisualRecor;
+    //private VisualRecTask visualRecTaskTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        String filePath = getIntent().getStringExtra("imagePath");
-        Log.d(TAG, "get path: " + filePath);
+        filePath = getIntent().getStringExtra("imagePath");
+        Log.d(TAG, "get path1: " + filePath);
 
 
         mImage = findViewById(R.id.image);
+        mTextView = findViewById( R.id.tv_visual_rec );
+        mButton = findViewById( R.id.btn_visualrec );
+        mButton.setOnClickListener(this);
 
         Bitmap image = BitmapFactory.decodeFile(filePath);
 
-        new File(filePath).delete();
+        //new File(filePath).delete();
 
         Log.d(TAG, "file size : " + image.getByteCount());
         mImage.setImageBitmap(image);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_visualrec:
+                // String filePath = "file:" + getIntent().getStringExtra("imagePath");
+                Log.d(TAG, "get path: " + filePath);
+
+                new VisualRecTask( mTextView ).execute(filePath);
+               // new File(filePath).delete();
+
+                break;
+        }
     }
 }
--- app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/util/VisualRecTask.java
+++ app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/util/VisualRecTask.java
@@ -1,4 +1,89 @@
 package edu.osu.cse5914.ibmi.foodrecommendation.util;
 
-public class VisualRecTask {
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+
+import edu.osu.cse5914.ibmi.foodrecommendation.CameraActivity;
+
+public class VisualRecTask extends AsyncTask<String, Void, String> {
+    protected TextView mText;
+    protected VisualRecognition mVisualRecor;
+
+    public VisualRecTask(TextView tv) {
+        mText = tv;
+
+        IamOptions options = new IamOptions.Builder()
+                .apiKey("gqPKKIDPqLEkA0qprEYRT8-spN3QdFTH5tE8SFtvQz_V")
+                .build();
+
+        mVisualRecor = new VisualRecognition("2018-03-19", options);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected String doInBackground(String... objects) {
+
+        Log.d("VisualRecTask", "File Place: " + objects[0]);
+        //final File photoFile = new File( (Uri.parse(objects[0])).getPath());
+        final File photoFile = new File(objects[0]);
+        //final File photoFile = new File("C:\\Users\\Ziming Ma\\Desktop\\Steak\\5.jpg");
+        Log.d("VisualRecTask", "Current File: " + photoFile);
+
+        InputStream imagesStream = null;
+        try {
+            imagesStream = new FileInputStream(photoFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Log.d("VisualRecTask", "Current File: " + imagesStream);
+        ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
+                .imagesFile(imagesStream)
+                .imagesFilename(photoFile.getName())
+                //.url("http://e.hiphotos.baidu.com/nuomi/pic/item/b64543a98226cffc07155532b1014a90f603ea77.jpg")
+                .classifierIds( Collections.singletonList("DefaultCustomModel_1496929061"))
+                .threshold((float) 0.6)
+                .owners( Arrays.asList("me"))
+                .build();
+        ClassifiedImages result = mVisualRecor.classify(classifyOptions).execute();
+
+
+        Log.d("VisualRecTask", "Current File: " + result.toString());
+        return result.getImages().get(0).getClassifiers().get(0).getClasses().get(0).getClassName();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void[] values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String o) {
+        super.onPostExecute(o);
+
+        mText.setText(o);
+    }
 }
+
