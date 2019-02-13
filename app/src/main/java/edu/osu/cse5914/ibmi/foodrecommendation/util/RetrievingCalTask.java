356eaebd70d59c7f592f5a5dc472765a6d294530
RingSky <zhaozhiyuan95@gmail.com>
2019-02-12 22:57:14 -0500

    Copy of discovery task.

---
+++ app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/util/RetrievingCalTask.java
@@ -0,0 +1,56 @@
+package edu.osu.cse5914.ibmi.foodrecommendation.util;
+
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.widget.TextView;
+
+import com.ibm.watson.developer_cloud.discovery.v1.Discovery;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+
+public class RetrievingCalTask extends AsyncTask<String, Void, String> {
+    protected TextView mText;
+    protected Discovery mDiscovery;
+    protected String environmentId;
+    protected String collectionId;
+    protected String documentJson;
+
+    public RetrievingCalTask(TextView tv) {
+        mText = tv;
+
+        mDiscovery = new Discovery("2018-12-03");
+        mDiscovery.setEndPoint("https://gateway.watsonplatform.net/discovery/api");
+
+        IamOptions options = new IamOptions.Builder()
+                .apiKey("1YJgxYDcHqAH75k-Q3Z1_LKMegZa30d4gKAGEr_hiKKA")
+                .build();
+        mDiscovery.setIamCredentials(options);
+        environmentId = "d73e9ac3-e284-4c40-b672-bb0a228a3e81";
+        collectionId = "27e5c66c-09cd-4cd8-b221-cabbb18e5bf7";
+    }
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected String doInBackground(String... objects) {
+        InputStream documentStream = new ByteArrayInputStream(documentJson.getBytes());
+        return null;
+    }
+    @Override
+    protected void onProgressUpdate(Void[] values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String o) {
+        super.onPostExecute(o);
+        mText.setText(o);
+    }
+
+}
