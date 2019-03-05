365ed5020f8ff4e1abd5cc17b19e9dd807a23015
Chen Bin <chen.6133@osu.edu>
2019-03-04 19:23:03 -0500

    Update .gitignore

---
+++ app/src/main/java/edu/osu/cse5914/ibmi/foodrecommendation/data/ProjectApi.java
@@ -0,0 +1,6 @@
+package edu.osu.cse5914.ibmi.foodrecommendation.data;
+
+public class Api {
+    static String FIREBASE_API = "";
+    static String DISCOVERY_API = "U5YchTlhMNYISHMGcatpiEwk5pj4cmTzOP7cL2JTKnDK";
+}
